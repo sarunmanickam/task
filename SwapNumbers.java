152272874ba4b6a7b4b0271b01f40c355d28b03a
sarunmanickam <sarunmanickam@gmail.com>
2017-02-25 12:19:47 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,24 @@
+package Token;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10, b=30;
+System.out.println("before swaping");
+System.out.println("a="+a);
+System.out.println("b="+b);
+int c;
+c=b;
+b=a;
+a=c;
+
+System.out.println("after swaping");
+System.out.println("a="+a);
+System.out.println("b="+b);
+
+
+
+	}
+
+}
