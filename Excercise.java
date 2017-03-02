4546fbfbdea0c8ec516857a2f2f3e38fa85d0299
sarunmanickam <sarunmanickam@gmail.com>
2017-03-02 21:30:03 +0530

    Add files via upload

---
+++ Excercise.java
@@ -0,0 +1,18 @@
+
+public class Excercise {
+
+	public static void main(String[] args) {
+		int a,b;
+		for (a=0;a<=3;a++)
+		{
+			for(b=1;b<=3-a;b++)
+			{
+				System.out.print("x");
+			}
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
