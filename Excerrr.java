d75da3d26d8fe04702d81713e27513f81911d8fa
sarunmanickam <sarunmanickam@gmail.com>
2017-03-02 21:31:14 +0530

    Add files via upload

---
+++ Excerrr.java
@@ -0,0 +1,21 @@
+
+public class Excerrr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		for(a=1;a<=6;a++)
+		{
+			for (c=1;c<=a;c++)
+			System.out.print("x");
+		
+		for (b=1;b<=a;b++)
+		
+			System.out.print(b);
+			System.out.println();
+		}
+		
+
+	}
+
+}
