51b050c2f4c488d1ccdfa5a38bc9242238cbf05d
sarunmanickam <sarunmanickam@gmail.com>
2017-03-02 21:33:46 +0530

    Add files via upload

---
+++ Newww.java
@@ -0,0 +1,46 @@
+package Token;
+
+public class Newww {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int a=0,b;
+		for (b=1;b<=5;b++)//0246810
+		{
+			a=b*5;
+
+			System.out.println(b + "*" + 5 + "="  +a);
+
+		}*/
+		multiplll();
+		iffe();
+
+	}
+
+
+	static void multiplll() {
+		/*int c=1,d;
+		System.out.println("multiple of 9");
+		for(d=1;d<=5;d++);
+		{
+			c=d*9;
+
+			System.out.println(d+"*"+9+"="+c);
+		}
+		}*/
+
+	}
+	
+		static void iffe()
+		{
+
+			int a=5,b=10,c=15;
+			if (a>b)
+			{
+			System.out.println("hello");	
+			}
+
+		}
+	}
+
+
