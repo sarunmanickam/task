fedb48c7fbadeb7d49869926e30a1bbc3d40e499
sarunmanickam <sarunmanickam@gmail.com>
2017-03-02 21:29:19 +0530

    Add files via upload

---
+++ Exccer.java
@@ -0,0 +1,24 @@
+
+public class Exccer {
+
+	public static void main(String[] args) {
+		int a,b,c;
+
+		for (a=1;a<=7;a++)
+		{
+			for (b=1;b<=a;b++)
+			{
+				System.out.print(b);
+			}
+			for (c=7-a;c>=1;c--)//6
+			{
+				System.out.print("x");
+			}
+			System.out.println();
+
+			// TODO Auto-generated method stub
+
+		}}
+
+}
+
