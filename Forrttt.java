4b3f34d1e5d66128335b3aa7ef5d6f68899fbf79
sarunmanickam <sarunmanickam@gmail.com>
2017-03-02 20:39:32 +0530

    Add files via upload

---
+++ Forrttt.java
@@ -0,0 +1,22 @@
+
+public class Forrttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		for(a=1;a<=4;a++)
+		{ 
+			for(b=0;b<=3-a;b++)
+				System.out.print(".");
+			
+				for (c=1;c<=a;c++)
+				System.out.print("x");
+			System.out.println();
+			
+		}
+			
+		
+
+	}
+
+}
